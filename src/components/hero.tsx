21872a3ce6fd474e97b30dd23fbd2c2bb8fe97ad
eshaadeel0967 <eshaadeel754@gmail.com>
2024-11-11 16:50:15 +0500

    add

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -1,9 +1,16 @@
 import React from "react";
 import Image from "next/image";
-export default function Hero(){
-    return(
+export default function Hero() {
+    return (
         <div>
-            <Image src="/images/PP.jpg" width="300" height="300" alt="profile picture" className=""/>
+            <div className="flex">
+                <Image src="/images/PP.jpg" width="300" height="300" alt="profile picture" className="rounded-full " />
+                <h2 className="text-7xl text-left font-bold text-pink-500 ml-40 " >Hello! <br /> I am <br /> Esha </h2>
+            </div>
+            <div className="ml-14 flex gap-14 mt-14">
+                <button className="bg-gray-100 border-0 py-1 px-3 text-gray-500 focus:outline-none hover:bg-pink-300 rounded text-base mt-4 md:mt-0">About me </button>
+                <button className="bg-gray-100 border-0 py-1 px-3 text-gray-500 focus:outline-none hover:bg-pink-300 rounded text-base mt-4 md:mt-0">Contact me</button>
+            </div>
         </div>
     )
 }
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -4,16 +4,16 @@ export default function Navbar(){
         <header className="text-gray-600 body-font">
   <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
       <Link href="" className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
-      <h2 className="text-3xl font-bold text-blue-500 hover:text-blue-300 hover:underline">Esha</h2>
+      <h2 className="text-3xl font-bold text-white hover:text-pink-300 hover:underline">Esha</h2>
     </Link>
     <nav className="md:ml-auto md:mr-auto flex flex-wrap items-center text-base justify-center">
-      <Link href="" className="mr-5 text-white hover:text-gray-300">Home</Link>
-      <Link href="" className="mr-5 text-white hover:text-gray-300">About</Link>
-      <Link href="" className="mr-5 text-white hover:text-gray-300">Skills</Link>
-      <Link href="" className="mr-5 text-white hover:text-gray-300">Contact</Link>
+      <Link href="" className="mr-5 text-white hover:text-pink-300">Home</Link>
+      <Link href="" className="mr-5 text-white hover:text-pink-300">About</Link>
+      <Link href="" className="mr-5 text-white hover:text-pink-300">Skills</Link>
+      <Link href="" className="mr-5 text-white hover:text-pink-300">Contact</Link>
     </nav>
-    <button className="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">
-      Button
+    <button className="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-pink-300 rounded text-base mt-4 md:mt-0">
+      Download CV
       <svg
         fill="none"
         stroke="currentColor"
